94c7508840df6f3e7695f2b23bf27d716bde824c
halurabiyyu <halurabiyyu@gmail.com>
2024-05-14 10:36:11 +0700

    JS 12 : E. Membuat SYMLINK

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -18,9 +18,11 @@ class FileUploadController extends Controller
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-' . time() . "." . $extfile;
         // $namaFile = $request->berkas->getClientOriginalName();
-        $path = $request->berkas->storeAs('uploads', $namaFile);
-        echo "proses upload berhasil, file berada di: $path";
-        
+        $path = $request->berkas->storeAs('public', $namaFile);
+        $pathBaru = asset('storage/'. $namaFile);
+        echo "proses upload berhasil, data disimpan pada: $path";
+        echo "<br>";
+        echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
         // echo $request->berkas->getClientOriginalName(). " lolos validasi";
         // dump($request->berkas);
         // return "Pemrosesan file upload di sini";
