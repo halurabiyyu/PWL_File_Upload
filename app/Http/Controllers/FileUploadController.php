781aa5014830e10ee4542464452bdc2332c27d79
halurabiyyu <halurabiyyu@gmail.com>
2024-05-14 10:05:24 +0700

    JS 12 : C. Validasi File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,23 +11,27 @@ class FileUploadController extends Controller
     }
 
     public function prosesFileUpload(Request $request){
+        $request->validate([
+            'berkas'=>'required|file|image|max:5000',
+        ]);
+        echo $request->berkas->getClientOriginalName(). " lolos validasi";
         // dump($request->berkas);
         // return "Pemrosesan file upload di sini";
-        if ($request->hasFile('berkas')) {
-            echo "path(): ". $request->berkas->path();
-            echo "<br>";
-            echo "extension(): ". $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ". $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ". $request->berkas->getSize();
-        }
-        else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // if ($request->hasFile('berkas')) {
+        //     echo "path(): ". $request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension(): ". $request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): ". $request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): ". $request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): ". $request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): ". $request->berkas->getSize();
+        // }
+        // else {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
     }
 }
